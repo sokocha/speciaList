6f830f1aa60739f31087f90fb76b6f8bb929e97c
Sadiq Okocha <sokocha@gmail.com>
2015-02-07 17:33:37 +0000

    changes root to be contractors#index instead of users#index

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users
-  root to: "users#index"
+  root to: "contractors#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
