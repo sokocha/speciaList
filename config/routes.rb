9d1b95e2c7a71e68ccabdb878d9a040df2a0a6a3
Sadiq Okocha <sokocha@gmail.com>
2015-02-07 15:57:52 +0000

    runs rails generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users
   root to: "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
