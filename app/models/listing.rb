34206e9920d3775eac3e8b2faf5761e796777ce2
Sadiq Okocha <sokocha@gmail.com>
2015-02-08 14:19:10 +0000

    adds validations for listings and offers

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,18 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   has_many :offers
   has_one :booking
+
+  validates :title, presence: true
+  validates :budget, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+  validates :category_id, presence: true
+  validates :user_id, presence: true
+
+  validates :title, length:{minimum: 3 }
+  validates :budget, :numericality => { :greater_than_or_equal_to => 1 }
+  validates :description, length:{minimum: 75}
+  
+
+
 end
