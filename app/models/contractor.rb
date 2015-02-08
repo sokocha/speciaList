3eaf145850bb68c19d4403d0d3a6ef076a1ce88b
Sadiq Okocha <sokocha@gmail.com>
2015-02-08 14:23:34 +0000

    adds validations for contractor's wage, user_id and category_id

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -1,3 +1,10 @@
 class Contractor < ActiveRecord::Base
   belongs_to :user
+
+  validates :wage, presence: true
+  validates :user_id, presence: true
+  validates :category_id, presence: true
+
+
+  validates :wage, :numericality => { :greater_than_or_equal_to => 1 }
 end
