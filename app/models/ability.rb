bd1120237d5be853203023e5862663f6f460b0df
Sadiq Okocha <sokocha@gmail.com>
2015-02-08 17:41:27 +0000

    defines permissions in abilities controller, though still not working

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,33 +2,39 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-      user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      else
+    user ||= User.new
+
+    if user.role? :admin
+      can :manage, :all
+
+    elsif user.persisted?
+      can :read, :all
+      can :create, Contractor
+      can :create, Offer
+      can :create, Listing
+      can :create, Booking 
+      cannot :create, User
+      cannot :create, Category
+
+      can :destroy, Offer
+      can :destroy, Listing
+      cannot :destroy, Contractor
+      cannot :destroy, User
+      cannot :destroy, Category
+
+      can :update, Offer
+      can :update, Contractor
+      can :update, Listing
+      cannot :update, Booking
+      can :update, User
+      cannot :update, Category
+
+    else
+        # what can non-logged-in users do?
         can :read, :all
-        cannot :update, Listing
-        cannot :update, Booking
-      end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    end
+    
   end
+  
+
 end
