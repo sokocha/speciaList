c1a0502814fc454da9a02fcb825f5f83d7851abc
Sadiq Okocha <sokocha@gmail.com>
2015-02-10 21:05:45 +0000

    makes phone number a unique field in users table

---
+++ db/migrate/20150210210411_remove_phone_number_again_from_users.rb
@@ -0,0 +1,5 @@
+class RemovePhoneNumberAgainFromUsers < ActiveRecord::Migration
+  def change
+    remove_column :users, :phone_number, :string
+  end
+end
---
+++ db/migrate/20150210210448_add_phone_number_again_to_users.rb
@@ -0,0 +1,6 @@
+class AddPhoneNumberAgainToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :phone_number, :string
+    add_index :users, :phone_number, unique: true
+  end
+end
