3dd62d3b9698ff1469591938533e8793a2d665d2
Sadiq Okocha <sokocha@gmail.com>
2015-02-13 13:46:41 +0000

    creates comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,45 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, except: [:new, :create]
+  before_action :set_booking
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.build_from(@booking, current_user.id, params[:comment][:body])
+    # @comment.title = params[:comment][:title] 
+
+    if @comment.save
+      redirect_to @booking
+    else
+      render :js => "alert('error saving comment');"
+    end
+  end
+
+  def destroy
+    if @comment.destroy
+      render :json => @comment, :status => :ok
+    else
+      render :js => "alert('error deleting comment');"
+    end
+  end
+
+  private
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def prepare_new_comment
+    @comment = Comment.new(commentable_id: @booking.id, commentable_type: @booking.constantize)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:title, :body, :commentable_id, :commentable_type)
+  end
+  
+end
