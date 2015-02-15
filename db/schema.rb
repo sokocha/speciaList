48ebfb5afbe6c731f1d4bc02cb83f8e0e793c75e
Sadiq Okocha <sokocha@gmail.com>
2015-02-15 10:33:32 +0000

    migrates user_avatar to Users table for carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214152135) do
+ActiveRecord::Schema.define(version: 20150215103222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 20150214152135) do
     t.inet     "last_sign_in_ip"
     t.string   "role"
     t.string   "phone_number"
+    t.string   "user_avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
