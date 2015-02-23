88c5aaf11bda1425edd83fe67600548f696ba6de
Sadiq Okocha <sokocha@gmail.com>
2015-02-23 15:28:08 +0000

    comments kaminari config file

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,5 +1,5 @@
-Kaminari.configure do |config|
-  config.default_per_page = 5
+# Kaminari.configure do |config|
+#   config.default_per_page = 5
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
@@ -7,4 +7,4 @@ Kaminari.configure do |config|
   # config.right = 0
   # config.page_method_name = :page
   # config.param_name = :page
-end
+# end
