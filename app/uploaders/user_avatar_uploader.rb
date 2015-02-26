fb424d1db1214aae723315e6107314a7f910cba9
Sadiq Okocha <sokocha@gmail.com>
2015-02-26 15:47:17 +0000

    allows for a larger size of thumb images

--- app/uploaders/user_avatar_uploader.rb
+++ app/uploaders/user_avatar_uploader.rb
@@ -33,7 +33,7 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [50, 50]
+    process :resize_to_fill => [900, 900]
   end
 
   version :detail do
