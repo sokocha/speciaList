91caf40c1972b22f0b1ca1c795e351a846d522dd
Sadiq Okocha <sokocha@gmail.com>
2015-02-27 16:52:45 +0000

    edited the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,9 +26,9 @@ normal_user1 = User.create!(first_name: 'James', last_name: 'fajimolu', address:
 
 normal_user2 = User.create!(first_name: 'Chudi', last_name: 'Ngonadi', address: '33 alfred rewane',phone_number: '07458720993', email: "user1@specialist.com", password: 'password', role: "user", user_avatar: 'http://dev.sol-technology.com.au/QUT/images/profile/tempProfile1.jpg')
 
-contractor_admin = Contractor.create!(user_id: admin_user.id, wage: 235, category_id: Category.first.id)
-contractor_1 = Contractor.create!(user_id: normal_user1.id, wage: 2114, category_id: Category.last.id) 
-contractor_2 = Contractor.create!(user_id: normal_user2.id, wage: 2114, category_id: Category.first(2).last.id)
+contractor_admin = Contractor.create!(user_id: admin_user.id, wage: 235, category_id: Category.first.id, verified: true, certified: true)
+contractor_1 = Contractor.create!(user_id: normal_user1.id, wage: 2114, category_id: Category.last.id, verified: true, certified: false) 
+contractor_2 = Contractor.create!(user_id: normal_user2.id, wage: 2114, category_id: Category.first(2).last.id, verified:true, certified:false)
 
 
 
