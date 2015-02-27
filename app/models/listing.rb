81a8e6adabdc641b56272915fcd581ced4549e74
Sadiq Okocha <sokocha@gmail.com>
2015-02-27 23:50:32 +0000

    runs rake db:seed

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -19,33 +19,33 @@ class Listing < ActiveRecord::Base
   validates :budget, :numericality => { :greater_than_or_equal_to => 1 }
   validates :description, length:{minimum: 10}
 
-  after_create :send_new_listing_sms
+    after_create :send_new_listing_sms
 
-  def send_new_listing_sms
-    client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token, Rails.application.secrets.twilio_phone_number
+    def send_new_listing_sms
+      client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token, Rails.application.secrets.twilio_phone_number
 
 
-    # relevant_contractors_phone_numbers = Contractor.where(category_id: self.category.id).map {|contractor| contractor.user.phone_number}.join(', ')}
+      # relevant_contractors_phone_numbers = Contractor.where(category_id: self.category.id).map {|contractor| contractor.user.phone_number}.join(', ')}
 
-    # What is the best way to put the listing id into this sms, so that the listing id remains included in the contractor response? Use REGEXP? 
+      # What is the best way to put the listing id into this sms, so that the listing id remains included in the contractor response? Use REGEXP? 
 
-# SENDS OUT SMS TO ALL CONTRACTORS WITHIN A CATEGORY,EXCEPT THE LISTING CREATOR
-    self.category.contractors.where(status: 'active').map do |contractor|
-      # unless contractor.user == self.user
-      message = client.messages.create from: ENV["TWILIO_PHONE_NUMBER"], to: contractor.user.phone_number, body: "#{self.user.first_name} #{self.user.last_name} is paying ₦#{self.budget} for a #{self.category.name}. Description: #{self.description}. If interested, reply this message with the following format listing#{self.id}#your_offer_in_naira_here"
-      # end 
+  # SENDS OUT SMS TO ALL CONTRACTORS WITHIN A CATEGORY,EXCEPT THE LISTING CREATOR
+      self.category.contractors.where(status: 'active').map do |contractor|
+        # unless contractor.user == self.user
+        message = client.messages.create from: ENV["TWILIO_PHONE_NUMBER"], to: contractor.user.phone_number, body: "#{self.user.first_name} #{self.user.last_name} is paying ₦#{self.budget} for a #{self.category.name}. Description: #{self.description}. If interested, reply this message with the following format listing#{self.id}#your_offer_in_naira_here"
+        # end 
+      end
+
+
+
+
+
+     
+      
+  # Send active listing status to the creator of the listing
+      message = client.messages.create from: ENV["TWILIO_PHONE_NUMBER"], to: self.user.phone_number, body: "#{self.user.first_name}, your listing: #{self.title} for ₦#{self.budget} is now active. You should receive offers from interested contractors shortly" 
+
     end
-
-
-
-
-
-   
-    
-# Send active listing status to the creator of the listing
-    message = client.messages.create from: ENV["TWILIO_PHONE_NUMBER"], to: self.user.phone_number, body: "#{self.user.first_name}, your listing: #{self.title} for ₦#{self.budget} is now active. You should receive offers from interested contractors shortly" 
-
-  end
 
   # def open?
   #   offers.any? {|o| o.status == 'accepted'}
