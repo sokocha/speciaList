d9277a708c9eac1066525cec5867dd839093baf3
Sadiq Okocha <sokocha@gmail.com>
2015-02-27 23:19:03 +0000

    adds listings faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,9 +29,76 @@ normal_user2 = User.create!(first_name: 'Chudi', last_name: 'Ngonadi', address: 
 
 normal_user3 = User.create!(first_name:Faker::Name.first_name, last_name: Faker::Name.first_name, address: Faker::Address.street_address, phone_number: Faker::Number.number(11), email: Faker::Internet.safe_email, password: 'password', role: 'user', user_avatar: Faker::Avatar.image)
 
+normal_user4 = User.create!(first_name:Faker::Name.first_name, last_name: Faker::Name.first_name, address: Faker::Address.street_address, phone_number: Faker::Number.number(11), email: Faker::Internet.safe_email, password: 'password', role: 'user', user_avatar: Faker::Avatar.image)
+
+normal_user5 = User.create!(first_name:Faker::Name.first_name, last_name: Faker::Name.first_name, address: Faker::Address.street_address, phone_number: Faker::Number.number(11), email: Faker::Internet.safe_email, password: 'password', role: 'user', user_avatar: Faker::Avatar.image)
+
+normal_user6 = User.create!(first_name:Faker::Name.first_name, last_name: Faker::Name.first_name, address: Faker::Address.street_address, phone_number: Faker::Number.number(11), email: Faker::Internet.safe_email, password: 'password', role: 'user', user_avatar: Faker::Avatar.image)
+
+normal_user7 = User.create!(first_name:Faker::Name.first_name, last_name: Faker::Name.first_name, address: Faker::Address.street_address, phone_number: Faker::Number.number(11), email: Faker::Internet.safe_email, password: 'password', role: 'user', user_avatar: Faker::Avatar.image)
+
+normal_user8 = User.create!(first_name:Faker::Name.first_name, last_name: Faker::Name.first_name, address: Faker::Address.street_address, phone_number: Faker::Number.number(11), email: Faker::Internet.safe_email, password: 'password', role: 'user', user_avatar: Faker::Avatar.image)
+
+normal_user9 = User.create!(first_name:Faker::Name.first_name, last_name: Faker::Name.first_name, address: Faker::Address.street_address, phone_number: Faker::Number.number(11), email: Faker::Internet.safe_email, password: 'password', role: 'user', user_avatar: Faker::Avatar.image)
+
+normal_user10 = User.create!(first_name:Faker::Name.first_name, last_name: Faker::Name.first_name, address: Faker::Address.street_address, phone_number: Faker::Number.number(11), email: Faker::Internet.safe_email, password: 'password', role: 'user', user_avatar: Faker::Avatar.image)
+
 contractor_admin = Contractor.create!(user_id: admin_user.id, wage: 235, category_id: Category.first.id, verified: true, certified: true)
+
 contractor_1 = Contractor.create!(user_id: normal_user1.id, wage: 2114, category_id: Category.last.id, verified: true, certified: false) 
+
 contractor_2 = Contractor.create!(user_id: normal_user2.id, wage: 2114, category_id: Category.first(2).last.id, verified:true, certified:false)
+
+contractor_3 = Contractor.create!(user_id: normal_user3.id, wage: Faker::Number.number(4), category_id: Category.first(3).last.id, verified:true, certified:true)
+
+contractor_4 = Contractor.create!(user_id: normal_user4.id, wage: Faker::Number.number(4), category_id: Category.first(4).last.id, verified:true, certified:true)
+
+contractor_5 = Contractor.create!(user_id: normal_user5.id, wage: Faker::Number.number(4), category_id: Category.first(5).last.id, verified:false, certified:false)
+
+contractor_6 = Contractor.create!(user_id: normal_user6.id, wage: Faker::Number.number(4), category_id: Category.first(6).last.id, verified:true, certified:false)
+
+contractor_7 = Contractor.create!(user_id: normal_user7.id, wage: Faker::Number.number(4), category_id: Category.first(1).last.id, verified:false, certified:false)
+
+contractor_8 = Contractor.create!(user_id: normal_user8.id, wage: Faker::Number.number(4), category_id: Category.first(2).last.id, verified:true, certified:true)
+
+contractor_9 = Contractor.create!(user_id: normal_user9.id, wage: Faker::Number.number(4), category_id: Category.first(3).last.id, verified:true, certified:false)
+
+contractor_10 = Contractor.create!(user_id: normal_user10.id, wage: Faker::Number.number(4), category_id: Category.first(4).last.id, verified:true, certified:false)
+
+
+listing_1 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_1.id , user_id: admin_user.id, listing_image: Faker::Company.logo)
+listing_2 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_2.id , user_id: admin_user.id, listing_image: Faker::Company.logo)
+listing_3 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_3.id , user_id: admin_user.id, listing_image: Faker::Company.logo)
+listing_4 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_2.id , user_id: normal_user1.id, listing_image: Faker::Company.logo)
+listing_5 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_3.id , user_id: normal_user1.id, listing_image: Faker::Company.logo)
+listing_6 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_4.id , user_id: normal_user1.id, listing_image: Faker::Company.logo)
+listing_7 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_3.id , user_id: normal_user2.id, listing_image: Faker::Company.logo)
+listing_8 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_4.id , user_id: normal_user2.id, listing_image: Faker::Company.logo)
+listing_9 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_5.id , user_id: normal_user2.id, listing_image: Faker::Company.logo)
+listing_10 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_4.id , user_id: normal_user3.id, listing_image: Faker::Company.logo)
+listing_11 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_5.id , user_id: normal_user3.id, listing_image: Faker::Company.logo)
+listing_12 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_6.id , user_id: normal_user3.id, listing_image: Faker::Company.logo)
+listing_13 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_5.id , user_id: normal_user4.id, listing_image: Faker::Company.logo)
+listing_14 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_6.id , user_id: normal_user4.id, listing_image: Faker::Company.logo)
+listing_15 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_1.id , user_id: normal_user4.id, listing_image: Faker::Company.logo)
+listing_16 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_6.id , user_id: normal_user5.id, listing_image: Faker::Company.logo)
+listing_17 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_1.id , user_id: normal_user5.id, listing_image: Faker::Company.logo)
+listing_18 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_2.id , user_id: normal_user5.id, listing_image: Faker::Company.logo)
+listing_19 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_1.id , user_id: normal_user6.id, listing_image: Faker::Company.logo)
+listing_20 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_2.id , user_id: normal_user6.id, listing_image: Faker::Company.logo)
+listing_21 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_3.id , user_id: normal_user6.id, listing_image: Faker::Company.logo)
+listing_22 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_2.id , user_id: normal_user7.id, listing_image: Faker::Company.logo)
+listing_23 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_3.id , user_id: normal_user7.id, listing_image: Faker::Company.logo)
+listing_24 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_4.id , user_id: normal_user7.id, listing_image: Faker::Company.logo)
+listing_25 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_3.id , user_id: normal_user8.id, listing_image: Faker::Company.logo)
+listing_26 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_4.id , user_id: normal_user8.id, listing_image: Faker::Company.logo)
+listing_27 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_5.id , user_id: normal_user8.id, listing_image: Faker::Company.logo)
+listing_28 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_4.id , user_id: normal_user9.id, listing_image: Faker::Company.logo)
+listing_29 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_5.id , user_id: normal_user9.id, listing_image: Faker::Company.logo)
+listing_30 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_6.id , user_id: normal_user9.id, listing_image: Faker::Company.logo)
+listing_31 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_5.id , user_id: normal_user10.id, listing_image: Faker::Company.logo)
+listing_32 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_6.id , user_id: normal_user10.id, listing_image: Faker::Company.logo)
+listing_33 = Listing.create!(title: Faker::Commerce.product_name , description:Faker::Lorem.paragraph , budget: Faker::Number.number(5) , category_id: category_1.id , user_id: normal_user10.id, listing_image: Faker::Company.logo)
 
 
 
