d81f4c7190f3c93d0ec41a1d374ce4584c5df18d
Sadiq Okocha <sokocha@gmail.com>
2015-02-28 13:24:32 +0000

    edits ratings controller. now includes booking in its actions

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,11 +1,11 @@
 class RatingsController < ApplicationController
   before_action :set_rating, only: [:show, :edit, :update, :destroy]
+  before_action :set_booking, only: [:new, :create, :index]
 
   respond_to :html
 
   def index
-    @ratings = Rating.all
-    respond_with(@ratings)
+    @ratings = @booking.ratings
   end
 
   def show
@@ -13,27 +13,46 @@ class RatingsController < ApplicationController
   end
 
   def new
-    @rating = Rating.new
-    respond_with(@rating)
+    @rating = @booking.ratings.new
   end
 
   def edit
   end
 
   def create
-    @rating = Rating.new(rating_params)
-    @rating.save
-    respond_with(@rating)
+
+    @rating = @booking.ratings.new(rating_params)
+    @rating.user = current_user
+
+   respond_to do |format|
+      if @rating.save
+        format.html { redirect_to @booking, notice: 'Rating was successfully created.' }
+        format.json { render :show, status: :created, location: @rating }
+      else
+        format.html { render :new }
+        format.json { render json: @rating.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-    @rating.update(rating_params)
-    respond_with(@rating)
+    respond_to do |format|
+      if @rating.update(rating_params)
+        format.html { redirect_to @booking, notice: 'Rating was successfully updated.' }
+        format.json { render :show, status: :ok, location: @rating }
+      else
+        format.html { render :edit }
+        format.json { render json: @rating.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
     @rating.destroy
-    respond_with(@rating)
+    respond_to do |format|
+      format.html { redirect_to @booking, notice: 'Rating was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
@@ -41,7 +60,11 @@ class RatingsController < ApplicationController
       @rating = Rating.find(params[:id])
     end
 
+    def set_booking
+      @booking = Booking.find(params[:booking_id])
+    end
+
     def rating_params
-      params.require(:rating).permit(:professionalism_rating, :quality_rating)
+      params.require(:rating).permit(:professionalism_rating, :quality_rating, :booking_id)
     end
 end
